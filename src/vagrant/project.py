d50748d2ce730d133a04eea3130bc9cbaed732d1
jolsendev <jolsendev@gmail.com>
2017-08-28 14:44:38 -0700

    updated some crud

--- src/vagrant/project.py
+++ src/vagrant/project.py
@@ -1,35 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
 app = Flask(__name__)
 
-# Fake Restaurants
-restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
 
-restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name': 'Blue Burgers', 'id': '2'},
-               {'name': 'Taco Hut', 'id': '3'}]
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+# show all restaurants+
 
-# Fake Menu Items
-items = [
-    {'name': 'Cheese Pizza', 'description': 'made with fresh cheese', 'price': '$5.99', 'course': 'Entree', 'id': '1'},
-    {'name': 'Chocolate Cake', 'description': 'made with Dutch Chocolate', 'price': '$3.99', 'course': 'Dessert',
-     'id': '2'},
-    {'name': 'Caesar Salad', 'description': 'with fresh organic vegetables', 'price': '$5.99', 'course': 'Entree',
-     'id': '3'}, {'name': 'Iced Tea', 'description': 'with lemon', 'price': '$.99', 'course': 'Beverage', 'id': '4'},
-    {'name': 'Spinach Dip', 'description': 'creamy dip with fresh spinach', 'price': '$1.99', 'course': 'Appetizer',
-     'id': '5'}]
-item = {'name': 'Cheese Pizza', 'description': 'made with fresh cheese', 'price': '$5.99', 'course': 'Entree'}
-
-
-# engine = create_engine('sqlite:///restaurantmenu.db')
-# Base.metadata.bind = engine
-#
-# DBSession = sessionmaker(bind=engine)
-# session = DBSession()
-
-# show all restaurants
 @app.route('/')
 @app.route('/restaurants/')
 def all_restaurants():
+    restaurants = session.query(Restaurant).all()
     return render_template('restaurants.html', restaurants=restaurants)
 
 
@@ -54,13 +41,22 @@ def deleteRestaurant(restaurant_id):
 # show a restaurant menu
 @app.route('/restaurants/<int:restaurant_id>/menu/')
 def showMenu(restaurant_id):
-    return render_template('menu.html', menu_items=items)
+
+    return "Not Implemented" #render_template('menu.html', menu_items=items)
 
 
 # create a new menu item
 @app.route('/restaurants/<int:restaurant_id>/menu/new/')
 def newMenuItem(restaurant_id):
-    return render_template('newMenuItem.html')
+    if request.method == 'POST':
+        newItem = MenuItem(name=request.form['name'], description=request.form[
+            'description'], price=request.form['price'], course=request.form['course'], restaurant_id=restaurant_id)
+        session.add(newItem)
+        session.commit()
+        flash("new menu item created!")
+        return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('newMenuItem.html', restaurant_id=restaurant_id)
 
 
 # edit a menu item
@@ -77,4 +73,4 @@ def deleteMenuItem(restaurant_id, menu_id):
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8001)
